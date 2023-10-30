39bd41e3eea07eb2ee6bb9c255bd4630a1b18c45
ebikatsu <ennir0508+pg@gmail.com>
2023-10-30 20:27:54 +0900

    add base configs and maps

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -4,6 +4,15 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+
+    -- increment/decrement
+    ["+"] = { "<C-a>", "Increment", opts = { nowait = true } },
+    ["-"] = { "<C-x>", "Decrement", opts = { nowait = true } },
+
+    ["s"] = { "\"_s", "replace char without yank and change to insert mode" },
+    ["x"] = { "\"_x", "erase char without yank" },
+
+    ["<C-a>"] = { "gg<S-v>G", "Select All" },
   },
   v = {
     [">"] = { ">gv", "indent"},
