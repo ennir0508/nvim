f7332128bf4696bd17afb2aa227a2ef7b63d3f2f
ebikatsu <ennir0508+pg@gmail.com>
2023-11-03 20:35:10 +0900

    add general maps

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -9,13 +9,14 @@ M.general = {
     ["+"] = { "<C-a>", "Increment", opts = { nowait = true } },
     ["-"] = { "<C-x>", "Decrement", opts = { nowait = true } },
 
-    ["s"] = { "\"_s", "replace char without yank and change to insert mode" },
-    ["x"] = { "\"_x", "erase char without yank" },
+    ["s"] = { '"_s', "replace char without yank and change to insert mode" },
+    ["x"] = { '"_x', "erase char without yank" },
+    ["<BS>"] = { '"_xh', "Backspace is enable in nomal mode" },
 
     ["<C-a>"] = { "gg<S-v>G", "Select All" },
   },
   v = {
-    [">"] = { ">gv", "indent"},
+    [">"] = { ">gv", "indent" },
   },
 }
 
